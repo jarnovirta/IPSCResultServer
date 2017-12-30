5de506f989594270a12b05691f7bbd9ef548cb59
Jarno <Jarno@DESKTOP-KCASG3R>
2017-12-30 18:43:51 +0200

    Project configuration done.

---
+++ IPSCResultServer/src/main/java/fi/ipsc_result_server/controller/ResultUploaderApiController.java
@@ -0,0 +1,14 @@
+package fi.ipsc_result_server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ResultUploaderApiController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String testMethod() {
+		System.out.println("testmethod");
+		return "test";
+	}
+}
