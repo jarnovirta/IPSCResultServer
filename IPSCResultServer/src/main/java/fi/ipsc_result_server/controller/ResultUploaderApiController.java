71ef5f8e6df6520c92fcc4c3bf64114e6050a76e
Jarno <Jarno@DESKTOP-KCASG3R>
2017-12-30 19:31:59 +0200

    Working test code, receives MatchScore JSON and sends it back.

--- IPSCResultServer/src/main/java/fi/ipsc_result_server/controller/ResultUploaderApiController.java
+++ IPSCResultServer/src/main/java/fi/ipsc_result_server/controller/ResultUploaderApiController.java
@@ -1,8 +1,12 @@
 package fi.ipsc_result_server.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import fi.ipsc_result_server.domain.MatchScore;
 
 @Controller
 public class ResultUploaderApiController {
@@ -10,5 +14,12 @@ public class ResultUploaderApiController {
 	public String testMethod() {
 		System.out.println("testmethod");
 		return "test";
+	}
+	@RequestMapping(value = "/api/matches", method = RequestMethod.POST)
+	public @ResponseBody MatchScore putMatch(@RequestBody MatchScore matchScore) {
+		System.out.println("PUT new match");
+		// TODO: save match;
+		
+		return matchScore;
 	}
 }
---
+++ IPSCResultServer/src/main/java/fi/ipsc_result_server/domain/Stage.java
@@ -0,0 +1,5 @@
+package fi.ipsc_result_server.domain;
+
+public class Stage {
+
+}
