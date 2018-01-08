959b6a58a030526da78508db8cbf7565950ecb1b
Jarno Virta <jarnovirta@users.noreply.github.com>
2018-01-08 17:25:18 +0200

    Added result data object classes

---
+++ IPSCResultServer/src/main/java/fi/ipsc_result_server/domain/ResultData/StageResultData.java
@@ -0,0 +1,27 @@
+package fi.ipsc_result_server.domain.ResultData;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class StageResultData implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
